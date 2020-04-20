e217e354cfdbcd0df57d9c0f97d440b2f28dccd1
james lawrence <jamdlaw@yahoo.com>
2020-04-20 15:15:38 -0700

    added relationship between orders and order products

--- ecomm_api/app/Order.php
+++ ecomm_api/app/Order.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\OrderProducts;
 
 class Order extends Model
 {
     protected $guarded = [];
+
+    public function products()
+    {
+        return $this->hasMany(orderProducts::class);
+    }
 }
--- ecomm_api/app/OrderProducts.php
+++ ecomm_api/app/OrderProducts.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Orders;
 
 class OrderProducts extends Model
 {
     protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
