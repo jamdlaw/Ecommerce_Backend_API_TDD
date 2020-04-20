feedb194b772636352e64afe46c3cb562ac10b39
james lawrence <jamdlaw@yahoo.com>
2020-04-20 15:14:24 -0700

    created order factory

---
+++ ecomm_api/database/factories/OrderFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Customer;
+use App\Order;
+use Faker\Generator as Faker;
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'customer_id' => factory(Customer::class),
+    ];
+});
