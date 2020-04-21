f5b8333dac8b846e27c5d81fc89ced625f2f6217
james lawrence <jamdlaw@yahoo.com>
2020-04-21 11:43:31 -0700

    added and fixed tests

--- ecomm_api/tests/Feature/OrderAPITest.php
+++ ecomm_api/tests/Feature/OrderAPITest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use App\Order;
 use App\Customer;
 use App\OrderProducts;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderTest extends TestCase
 {
@@ -35,12 +36,12 @@ class OrderTest extends TestCase
                                                 'order_id' => $order->id,
                                                 'product_id' => 2, 
                                                 'quantity' => 3, 
-                                                'price' => 10.00
+                                                'price' => 10.00,
                                             ]);
         
         $this->assertCount(1, OrderProducts::all()); 
 
-        $order = Order::where($order->id);
+        $order = Order::where('id', $order->id)->first();
         
         $this->assertEquals($order->products()->first()->id, $order->id);
         $this->assertEquals($order->products()->first()->product_id, 2);
@@ -50,6 +51,7 @@ class OrderTest extends TestCase
     /** @test */
     public function orders_can_be_pulled_be_pending_status_code()
     {
+    
         $order = factory(Order::class)->create(['status' => 'pending']);
         $orderWeDoNotWant = factory(Order::class)->create(['status' => 'processing']);
         $anotherOrderWeDoNotWant = factory(Order::class)->create(['status' => 'processing']);
@@ -58,11 +60,27 @@ class OrderTest extends TestCase
 
         $this->assertCount(1, $ordersToSend);
     }
+
+    /** @test */
+    public function pending_orders_can_be_pulled_from_api()
+    {
+        $this->withoutExceptionHandling();
+        $orderWeDoNotWant = factory(Order::class)->create(['status' => 'processing']);
+        $order = factory(Order::class)->create(['status' => 'pending']);
+        $anotherOrderWeDoNotWant = factory(Order::class)->create(['status' => 'processing']);
+
+        $response = $this->get('/api/orders');
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJson([
+                                ['id'=> $order->id ]
+        ]);
+    }
     private function data()
     {
         return [
             'customer_id' => factory(Customer::class),  
-            
+            'status' => "pending",
         ];
     }
 }
